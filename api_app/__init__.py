80245df1b3460daa11a328043d78ae8bfb8546f6
Mr Hanzla <mr.hanzla101@gmail.com>
2023-02-16 10:33:20 +0500

    simple factory function of main app

--- api_app/__init__.py
+++ api_app/__init__.py
@@ -1,1 +1,30 @@
 import os
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'api_app.sqlite')
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the config is passed in
+        app.config.from_mapping(test_config)
+
+    # endure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/hello')
+    def hello():
+        return '<h1>Hello, Brother from another mother, Here we go again</h1>'
+
+    return app
