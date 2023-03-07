542a7b413c75e5aa54445447dcfff44762abf8da
Mr Hanzla <mr.hanzla101@gmail.com>
2023-03-07 09:45:04 +0500

    upgrade version - tax info working with all values

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='api_app',
-    version='1.2.4',
+    version='1.2.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
