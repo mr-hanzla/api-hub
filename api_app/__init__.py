ec5c350e655c7afb15867c362fcddfe00eece3e3
Mr Hanzla <mr.hanzla101@gmail.com>
2023-05-13 01:06:46 +0500

    add imports and register resume blueprint

--- api_app/__init__.py
+++ api_app/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config=None):
     def hello():
         return '<h1>Chillax, <br> routes coming soon <br>tey tussi ravo stay tuned</h1>'
 
-    from . import db, auth, blog, tax_info
+    from . import db, auth, blog, tax_info, resume
 
     db.init_app(app)
 
@@ -37,5 +37,7 @@ def create_app(test_config=None):
     app.add_url_rule('/', endpoint='blog.index')
 
     app.register_blueprint(tax_info.bp)
+
+    app.register_blueprint(resume.bp)
 
     return app
